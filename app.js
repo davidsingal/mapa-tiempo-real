ed23beaae2d3bb7158944de819a5d52394479de4
David Inga <davidsingal@Davids-MacBook-Air.local>
2013-03-25 21:49:08 +0100

    almacen de usuarios

--- app.js
+++ app.js
@@ -1,37 +1,50 @@
-//Cargamos módulos Node
+// Cargamos módulos Node
 var express = require('express');
 var http = require('http');
 
-//Creamos aplicación y sockets
+// Creamos aplicación, servidor y sockets
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 var port = process.env.VCAP_APP_PORT || 5000; //AppFog usa este puerto
 
-//Configuramos, ver http://expressjs.com/api.html
+// Almacén de usuarios
+var users = [];
+
+// Configuramos la aplicación, ver http://expressjs.com/api.html
 app.configure(function() {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.use(express.static(__dirname + '/public'));
 });
 
-//Inicio
+// Routing
 app.get('/', function(req, res) {
     res.render('index', {
-        'title': 'Mapa en tiempo real'
+        'title': 'Mapa en tiempo real',
+        'users': users // 
     });
 });
 
-//Sockets io, ver http://socket.io/
+// Sockets io, ver http://socket.io/
 io.set('transports', ['xhr-polling']); //AppFog usa Nginx
 io.sockets.on('connection', function(socket) {
+    // Escuchamos datos
     socket.on('send:coords', function (data) {
-        console.log(data);
-        socket.broadcast.emit('load:coords', data);
+        var user = {
+            name: data.name,
+            coords: data.coords
+        };
+
+        // Almacenamos el usuario
+        users.push(user);
+
+        // Enviamos usuarios a front
+        socket.broadcast.emit('load:coords', user);
     });
 });
 
-//Creamos servidor
-server.listen(port, function() {
-    console.log('Servidor funcionando en http://localhost:' + port);
-});
+//Iniciamos servidor
+server.listen(port);
+
+console.log('Servidor funcionando en http://localhost:' + port);
--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,73 @@
 function onDocumentReady() {
-	//Cargamos socket
-	var socket = io.connect('/');
+	// Conectamos sockets con el dominio
+	var socket = io.connect(window.location.href);
 
-	//Coordenada inicial
-	var Madrid = [40.416944, -3.703611];
+	// Coordenada inicial, punto entre América y Europa
+	var defaultLatLng = [0, -23.818359375];
 
-	//Creamos mapa, ver http://leafletjs.com/examples/quick-start.html
+	// Creamos mapa, ver http://leafletjs.com/examples/quick-start.html
 	var map = L.map('map', { //Id de nuestro div
-		center: Madrid,
-		zoom: 4
+		center: defaultLatLng,
+		zoom: 3,
+		maxZoom: 18
 	});
 
-	//Creamos tiles y los añadimos al mapa, ver http://mapbox.com/
-	var layer = L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+	// Creamos tiles y los añadimos al mapa, algunos ejemplos, ver más en http://wiki.openstreetmap.org/wiki/Tileserver
+	// OpenStreetMaps Tiles: http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
+	// Cloudmade Tiles: http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png
+	// Toner Tiles: http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png
+	var layer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: 'David Inga',
 		maxZoom: 18
 	});
 
+	// Añdimos el layer creado al mapa
 	layer.addTo(map);
 
-	//Geolocalización, ver http://leafletjs.com/reference.html#map-locate-options
+	// Geolocalizamos al usuario, ver http://leafletjs.com/reference.html#map-locate-options
 	map.locate({
 		enableHighAccuracy: true
 	});
 
-	//Creamos marker para ponerlo en nuestra posición
+	// Creamos marker para ponerlo en nuestra posición
 	map.on('locationfound', onLocationFound);
 
-	//Creamos marker cuando nos llega una coordenada
+	// Creamos marker cuando nos llega una coordenada
 	socket.on('load:coords', onLoadCoords);
 
 	function onLocationFound(position) {
-		var myPosition = L.marker([position.latlng.lat, position.latlng.lng]);
-		myPosition.addTo(map);
-		socket.emit('send:coords', position.latlng);
+		// Preguntamos usuario de Twitter
+		var name = window.prompt('Introduce tu usuario de twitter', '@');
+		if (!name || name === '') name = 'anonymous';
+		var coords = [position.latlng.lat, position.latlng.lng];
+
+		createUser(name, coords);
+
+		socket.emit('send:coords', {
+			name: name,
+			coords: coords
+		});
 	}
 
-	function onLoadCoords(coord) {
-		console.log(coord);
-		var userMarker = L.marker([coord.lat, coord.lng]);
-		userMarker.addTo(map);
+	function onLoadCoords(data) {
+		createUser(data.name, data.coords);
+	}
+
+	function onLoadUsers(users) {
+		if (users.length > 0) return false;
+		for (var i = 0, len = users.length; i < len; i++) {
+			createUser(user[i].name, user[i].coords);
+		}
+	}
+
+	function createUser(name, coords) {
+		var user = {
+			name: name,
+			coords: coords
+		};
+		var marker = L.marker(coords);
+		marker.addTo(map);
+		marker.bindPopup(user.name);
 	}
 }
 
